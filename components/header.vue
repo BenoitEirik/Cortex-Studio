0f56e36cda9e4ecab9cc2ba6143b449e93b4aceb
BenoitEirik <olafsergent@gmail.com>
2024-02-02 07:26:09 +0100

    Improve header

--- components/header.vue
+++ components/header.vue
@@ -2,12 +2,15 @@
 <header class="fixed top-0 h-0 z-10 w-screen flex justify-center">
   <div class="absolute top-2 left-4 right-4 flex justify-center">
     <nav ref="headerRef"
-      class="max-w-7xl w-full max-h-[var(--var-header-height)] px-6 py-4 flex justify-between items-center rounded-3xl"
+      class="max-w-7xl w-full max-h-[var(--var-header-height)] flex justify-between items-center rounded-3xl"
       :style="{ 'background': scrolling ? 'rgba(17, 24, 39, 80%)' : 'none' }" :class="{ 'backdrop-blur': scrolling }">
-      <NuxtLink to="/">
-        <NuxtImg id="logo_header" width="200px" src="logo.png" alt="Cortex Studio logo" />
-      </NuxtLink>
-      <ul class="px-4 py-2 hidden lg:flex justify-end gap-4 text-2xl text-white font-['ClashDisplay-Bold']">
+      <div class="grow self-stretch pl-6 py-4 flex items-center rounded-l-3xl" @pointerover="closeMenu()">
+        <NuxtLink to="/">
+          <NuxtImg id="logo_header" width="200px" src="logo.png" alt="Cortex Studio logo" />
+        </NuxtLink>
+      </div>
+      <ul
+        class="shrink-0 px-4 py-6 pr-10 hidden lg:flex justify-end gap-4 text-2xl text-white font-['ClashDisplay-Bold'] rounded-r-3xl">
         <li>
           <NuxtLink to="/#notre-travail" class="font-bold" @pointerover="launchMenu('#notre-travail')"
             @click="closeMenu()">Notre travail</NuxtLink>
@@ -25,7 +28,7 @@
         </li>
       </ul>
       <div
-        class="px-4 py-2 lg:hidden flex justify-center items-center bg-[var(--var-color-bg)] border-2 border-gray-800 rounded-3xl cursor-pointer"
+        class="mx-4 my-4 px-4 py-2 lg:hidden flex justify-center items-center bg-[var(--var-color-bg)] border-2 border-gray-800 rounded-3xl cursor-pointer rounded-r-3xl"
         @click="launchMenu('#')">
         <Icon name="i-iconoir-menu" size="1.5rem" />
       </div>
@@ -151,4 +154,5 @@ nav {
   ul li {
     text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
   }
-}</style>
+}
+</style>
